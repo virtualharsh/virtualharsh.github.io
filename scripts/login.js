fc63eb72d64c500976b7e560a09eb771b690ad92
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-02-05 14:24:31 +0530

    added themes

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,23 @@
-class SecretLogin{
+class swapbg{
     static x = 0;
 }
 
-function Inc(){
-    SecretLogin.x++;
-    if(SecretLogin.x>=7){
-        window.location.replace("/admin.html");
+  
+function swap() {
+    if(swapbg.x==0){
+        document.body.style.background = 'white';
+        document.getElementById("nav").style.background = "white";
+        document.body.style.color = 'black';
+        document.getElementById('nav').style.color = 'black';
+        document.getElementById("btnclr").style.filter = "invert(100%)";
+        swapbg.x=1;
+
+    }else{
+        document.body.style.background = 'black';
+        document.getElementById("nav").style.background = "black";
+        document.body.style.color = 'white';
+        document.getElementById('nav').style.color = 'white';
+        document.getElementById("btnclr").style.filter = "invert(0%)";
+        swapbg.x=0;
     }
-}
+} 
