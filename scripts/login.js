2b103e20e3e781cd27d5e0bf88150dab5aa4d6a8
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-02-05 13:17:49 +0530

    added shortcut keys in navigation and added message when a device loads website for first time

--- scripts/login.js
+++ scripts/login.js
@@ -3,9 +3,17 @@ class SecretLogin{
 }
 
 function Inc(){
-    console.log("btn clicked");
     SecretLogin.x++;
     if(SecretLogin.x>=7){
         window.location.replace("/admin.html");
     }
 }
+
+
+
+function myFunction() {
+    if(document.cookie==null){
+        alert("This website looks amazing in laptops make sure to visit it once");
+        document.cookie = "Set";
+    }
+ }
