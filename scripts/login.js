4f42cf99fd334230f4f5a372e4b843df4b6dbfb6
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-02-05 13:48:08 +0530

    removed alert issue

--- scripts/login.js
+++ scripts/login.js
@@ -8,12 +8,3 @@ function Inc(){
         window.location.replace("/admin.html");
     }
 }
-
-
-
-function myFunction() {
-    if(document.cookie==null){
-        alert("This website looks amazing in laptops make sure to visit it once");
-        document.cookie = "Set";
-    }
- }
